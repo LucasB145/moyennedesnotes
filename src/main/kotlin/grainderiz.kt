a463bf8b3a970b1c62f1101bf66889651f7706f6
LucasB145 <99190894+LucasB145@users.noreply.github.com>
2022-07-05 11:30:40 +0200

    Initial commit

---
+++ src/main/kotlin/grainderiz.kt
@@ -0,0 +1,12 @@
+import java.math.BigInteger
+
+fun main(args: Array<String>) {
+    var grainderiz = BigInteger.valueOf(1);
+    var n = 2
+
+    println("Case 1 : $grainderiz")
+    for(i in 2..64){
+        grainderiz = grainderiz.times(n.toBigInteger())
+        println("Case $i : $grainderiz")
+    }
+}
